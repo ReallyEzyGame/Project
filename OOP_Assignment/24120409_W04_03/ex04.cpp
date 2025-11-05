556aaf29c480424fa7998fb080566d4ccba6fccf
ReallyEzyGame <lethanhphong010106@gmail.com>
2025-11-05 16:05:55 +0700

    the unfinished sourecode of the ex04 in homework OOP courses

--- OOP_Assignment/24120409_W04_03/ex04.cpp
+++ OOP_Assignment/24120409_W04_03/ex04.cpp
@@ -1,5 +1,19 @@
 #include <iostream>
 
-int main()
+template <typename T>
+class Node
 {
-}
+private:
+    Node *next;
+    T data;
+
+public:
+    Node();
+    Node(const Node *&);
+};
+template <typename T>
+class LinkedList
+{
+private:
+public:
+};
